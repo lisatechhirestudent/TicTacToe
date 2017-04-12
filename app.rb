3a3f5977fb0380acb53519cd8f0256cde98a8de0
techhirestudent <live_work_achieve@outlook.com>
2017-04-12 17:47:29 -0400

    TTTweb tested and functional

--- app.rb
+++ app.rb
@@ -1,14 +1,12 @@
 require "sinatra"
 require_relative "board.rb"
-require_relative "webappgame.rb"
+require_relative "human_console.rb" 
+require_relative "sequential.rb"
+require_relative "random.rb"
 enable :sessions
-
-
 
 get'/' do 
 board = Board.new
-
-
   erb :welcomettt, :locals=> {:board=>board.ttt_board}
 end
 
@@ -18,28 +16,53 @@ post '/player_1' do
 end
 get '/player_2' do
   p1_name = params[:p1_name]
-  erb :p2_name,locals: {p1_name: p1_name}
+  erb :p2_type,locals: {p1_name: p1_name}
+end
+post '/gamer' do
+	game_type=params[:game_type]
+	if game_type=="human"
+		session[:player_2] = HumanConsole.new("o")
+	elsif game_type=="sequential"
+	session[:player_2] = SequentialConsole.new("o")
+    elsif game_type=="random"
+	session[:player_2] = RandomConsole.new("o")
+	end
+	session[:p1_name] = params[:p1_name]
+	session[:board] = Board.new
+	session[:player_1] = HumanConsole.new("x")
+	
+	session[:current_player] = session[:player_1]
+#"#{session[:p1_name]} and #{session[:p2_name]}"
+	session[:message] = "Welcome"
+	redirect "/gameplayroute"
+
+
 end
 post '/player_2' do
 	session[:p1_name] = params[:p1_name]
-	session[:p2_name] = params[:p2_name]
+	session[:player_2] = SequentialConsole.new("o")
+	p2_name = params[:p2_name]
 	session[:board] = Board.new
 	session[:player_1] = HumanConsole.new("x")
-	session[:player_2] = HumanConsole.new("o")
+	
 	session[:current_player] = session[:player_1]
 #"#{session[:p1_name]} and #{session[:p2_name]}"
-	redirect '/gameplayroute'
+	session[:message] = "Welcome"
+	redirect "/gameplayroute"
 end
 
 get '/gameplayroute' do
-	erb :gameplayboard,locals: {board: session[:board].ttt_board} 
+	message= params[:message]
+	erb :gameplayboard,locals: {board: session[:board].ttt_board, message: message} 
 
 end
 post '/move' do 
 	move = params[:pick].to_i
-	if  session[:board].open_space?(move)
+	
+	if session[:board].open_space?(move)
 	    session[:board].update_board(move,session[:current_player].marker)
 	else
+		session[:message]="space_already_taken"
 		redirect '/gameplayroute'
 	end		
 	if  session[:board].board_win?(session[:current_player].marker) || session[:board].full_board?()
@@ -49,6 +72,22 @@ post '/move' do
 		   session[:current_player] = session[:player_2]
 		else
 		   session[:current_player] = session[:player_1]
+		end
+		session[:message] ="player_changed" 
+
+		if session[:current_player].class != HumanConsole
+			move = session[:current_player].get_move(session[:board].ttt_board)
+			session[:board].update_board(move,session[:current_player].marker)
+			if  session[:board].board_win?(session[:current_player].marker) || session[:board].full_board?()
+				redirect '/finish'
+			else 
+				if session[:current_player] == session[:player_1]
+		   			session[:current_player] = session[:player_2]
+				else
+		   			session[:current_player] = session[:player_1]
+				end
+				session[:message] ="player_changed" 
+			end
 		end
 		redirect '/gameplayroute'
 	end
